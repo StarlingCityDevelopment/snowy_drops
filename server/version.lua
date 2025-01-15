e13c57e40f9db908a704c73d0ead46dd82e18c7d
root <60280452+0Programmer@users.noreply.github.com>
2025-01-15 16:45:14 +0100

    chore(server/version): set recommended dependency to required

--- server/version.lua
+++ server/version.lua
@@ -1,8 +1,8 @@
 lib.versionCheck('SSnowly/snowy_drops')
 if not lib.checkDependency('ox_lib', '3.27.0', true) then
-    warn('ox_lib version 3.27.0 or higher is recommended')
-elseif not lib.checkDependency('ox_inventory', '2.43.4', true) then
-    warn('ox_inventory version 2.43.4 or higher is recommended')
+    error('ox_lib version 3.27.0 or higher is required')
+elseif not lib.checkDependency('ox_inventory', '2.43.5', true) then
+    error('ox_inventory version 2.43.5 or higher is required')
 elseif GetConvarInt('onesync_enableInfinity', 0) ~= 1 then
     error('OneSync Infinity is not enabled. You can do so in txAdmin settings or add +set onesync on to your server startup command line')
 end
